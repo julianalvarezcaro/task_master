5457ab5267d159e0fd6c2e66fc10fdde288eb1e3
Julian Alvarez <julian.alvarez.caro@hotmail.com>
2025-04-05 12:16:16 -0500

    Added Character view to admin page

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,5 @@
-from app.models import Task  # Make sure to use absolute import
+from app.tasks.models.character import Character
+from app.tasks.models.task import Task  # Make sure to use absolute import
 from django.contrib import admin
 
 @admin.register(Task)  # This decorator registers the model
@@ -6,3 +7,8 @@ class TaskAdmin(admin.ModelAdmin):
     list_display = ('user', 'title', 'description', 'created_at', 'is_completed', 'deleted_at', 'custom_fields')  # Fields to display
     list_filter = ('is_completed', 'created_at')  # Add filters
     search_fields = ('title', 'description')  # Add search functionality
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ('user', 'level', 'health', 'strength')
+    search_fields = ('user__username',)
