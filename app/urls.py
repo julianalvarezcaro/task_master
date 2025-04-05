210c8156e2e4c46215e05b87a289a7a345979ff7
Julian Alvarez <julian.alvarez.caro@hotmail.com>
2025-04-05 12:23:25 -0500

    Added character deatil view url and moved another

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,16 @@
-from app.views import RegisterView, TaskCompleteView, TaskCreateView, UserTaskListView
+from app.tasks.views.character_views import CharacterDetailView
+from app.tasks.views.task_views import (
+    TaskCompleteView,
+    TaskCreateView,
+    UserTaskListView
+)
+from app.tasks.views.user_views import RegisterView
 from django.urls import path
 
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='register'),
     path('tasks/', TaskCreateView.as_view(), name='task-create'),
+    path('tasks/<int:task_id>/complete/', TaskCompleteView.as_view(), name='task-complete'),
+    path('users/<int:user_id>/character/', CharacterDetailView.as_view(), name='character-detail'),
     path('users/<int:user_id>/tasks/', UserTaskListView.as_view(), name='user-tasks'),
-    path('api/tasks/<int:task_id>/complete/', TaskCompleteView.as_view(), name='task-complete'),
 ]
